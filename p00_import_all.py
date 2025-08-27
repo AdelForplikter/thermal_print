db080c0a889c168dd0aeede0a51623c9521c1cb2
kent <post@adelforplikter.no>
2025-08-27 19:09:38 +0200

    tiny dancer

--- p00_import_all.py
+++ p00_import_all.py
@@ -3,8 +3,10 @@ from p02_image_generation import *;
 
 
 
-def thermal_print(printer, headline, text, inspect_image_only=False, width=320):
-    print(f"Printing to {printer}: {headline} - {text}")
+# def thermal_print(printer, headline, text, inspect_image_only=False, width=320):
+#     print(f"Printing to {printer}: {headline} - {text}")
+# # thermal_print(printer="EPSON TM-T20III Receipt", headline="Klær",text="Sokker",inspect_image_only=True)
+
+create_image(headline="Mette Marit",text="Det var en gang en prinsesse\npå Quarts. Hennes far, Jon, var\nikke konge av landet.")
 
 
-thermal_print(printer="EPSON TM-T20III Receipt", headline="Klær",text="Sokker",inspect_image_only=True)
--- p01_print_thermal.py
+++ p01_print_thermal.py
@@ -1,16 +1,13 @@
 from escpos.printer import Win32Raw
 
-# """ Receipt Printer T20III Definitions (EPSON T20III Receipt) """
-Epson = Win32Raw("EPSON TM-T20III Receipt")
-# Epson = Win32Raw("POSPrinter POS80")
-
-
-Epson.set(custom_size=True, align='left', width=1, height=1)
-
-
-
 def print_sticker_to_thermal_printer(image_path):
     try:
+        # """ Receipt Printer T20III Definitions (EPSON T20III Receipt) """
+        Epson = Win32Raw("EPSON TM-T20III Receipt")
+        # Epson = Win32Raw("POSPrinter POS80")
+
+
+        Epson.set(custom_size=True, align='left', width=1, height=1)
         Epson._raw(b'\x1B\x40') # Initialize printer
         Epson.image(image_path, impl="bitImageRaster", center=True, high_density_horizontal=True, high_density_vertical=True)
         # Epson._raw(b'\x1D\x56\x00') # Cutting. Hard to implement
@@ -20,4 +17,4 @@ def print_sticker_to_thermal_printer(image_path):
         print(f"ERROR printing to thermal printer: {str(e)}")
 
 # print_sticker_to_thermal_printer("c:\\Users\\ano\\Downloads\\marta_klistre.png")
-print_sticker_to_thermal_printer("C:\\Users\\ano\\AppData\\Local\\Temp\\tmp9qf6f55w.png")
+# print_sticker_to_thermal_printer("C:\\Users\\ano\\AppData\\Local\\Temp\\tmp9qf6f55w.png")
